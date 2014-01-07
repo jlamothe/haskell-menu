68d8fa68fecb04d8626b6e136775c4c4ef1fc631
Jonathan Lamothe <jonathan@jlamothe.net>
2014-01-06 21:03:08 -0500

    Implemented doMenu

--- Menu.hs
+++ Menu.hs
@@ -3,32 +3,50 @@ module Menu ( Menu (..)
             , doMenu
             ) where
 
+import Data.Char
 import Data.Map (Map)
 import qualified Data.Map as Map
+import System.IO
 
 data Menu =
   Menu { title :: String
-       , menuItems :: Map Char Item
+       , items :: Map Char Item
        }
-  
+
 instance Show Menu where
   show menu =
-    "\n*** " ++ title menu ++ " ***\n" ++ showItems (menuItems menu)
+    "\n*** " ++ title menu ++ " ***\n" ++ showItems (items menu)
 
 showItems :: Map Char Item -> String
 showItems =
-  Map.foldrWithKey 
+  Map.foldrWithKey
   (\k x r ->
     k : ") " ++ show x ++ '\n' : r
   ) ""
-  
+
 data Item =
   Item { description :: String
        , action      :: IO ()
        }
-  
+
 instance Show Item where
   show = description
 
 doMenu :: Menu -> IO ()
-doMenu = undefined
+doMenu menu = do
+  hSetBuffering stdout NoBuffering
+  putStr $ show menu
+  menuPrompt menu
+
+menuPrompt :: Menu -> IO ()
+menuPrompt menu = do
+  putStr "Please make a selection: "
+  sel <- getLine
+  case sel of
+    (x : _) ->
+      case lookup (toUpper x) (items menu) of
+        Just item -> action item
+        _         -> menuPrompt menu
+    _ -> do
+      putStrLn "Invalid Selection"
+      menuPrompt menu
