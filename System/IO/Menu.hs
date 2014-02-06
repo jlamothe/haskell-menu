c6ebb6f67262d0e75b8ecbecf6848b0856be6ef0
Jonathan Lamothe <jonathan@jlamothe.net>
2014-02-06 14:09:33 -0500

    Moved Menu to System.IO.Menu

--- System/IO/Menu.hs
+++ System/IO/Menu.hs
@@ -16,7 +16,7 @@
 -- License along with this program.  If not, see
 -- <http://www.gnu.org/licenses/>.
 
-module Menu ( Menu (..)
+module System.IO.Menu ( Menu (..)
             , Item (..)
             , build
             , execute
