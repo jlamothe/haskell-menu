cfcc8e5d5379ce171ba6617043b7658d16641221
Jonathan Lamothe <jlamothe1980@gmail.com>
2016-12-06 13:52:05 -0500

    Added Haddock Documentation

--- System/IO/Menu.hs
+++ System/IO/Menu.hs
@@ -1,6 +1,6 @@
 -- Haskell Menu
 
--- Copyright (C) 2014 Jonathan Lamothe <jonathan@jlamothe.net>
+-- Copyright (C) 2014, 2016 Jonathan Lamothe <jlamothe1980@gmail.com>
 
 -- This program is free software: you can redistribute it and/or
 -- modify it under the terms of the GNU Lesser General Public License
@@ -16,6 +16,13 @@
 -- License along with this program.  If not, see
 -- <http://www.gnu.org/licenses/>.
 
+{-|
+Module      : System.IO.Menu
+Description : A simple menu system
+License     : LGPL-3
+Maintainer  : jlamothe1980@gmail.com
+-}
+
 module System.IO.Menu ( Menu (..)
                       , Item (..)
                       , build
@@ -28,9 +35,12 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import System.IO
 
+-- | Defines a 'Menu'
 data Menu a =
   Menu { title :: String
+       -- ^ The menu title
        , items :: Map Char (Item a)
+       -- ^ The items in the menu
        }
 
 instance Show (Menu a) where
@@ -44,26 +54,49 @@ showItems =
     k : ") " ++ show x ++ '\n' : r
   ) ""
 
+-- | Defines a 'Menu' item
 data Item a =
   Item { description :: String
+       -- ^ The description
        , action      :: IO a
        }
 
 instance Show (Item a) where
   show = description
 
-build :: String -> [(Char, Item a)] -> Menu a
+-- | Builds a 'Menu' from a list.
+build
+  :: String
+  -- ^ The menu title
+  -> [(Char, Item a)]
+  -- ^ A list of tuples containing a character (must be upper case)
+  -- and its corresponding 'Item'
+  -> Menu a
 build title items =
   Menu { title = title
        , items = Map.fromList items
        }
 
-execute :: Menu a -> IO a
+-- | Prompts the user to select an option from a 'Menu' and returns
+-- the resulting value
+execute
+  :: Menu a
+  -- ^ The 'Menu'
+  -> IO a
 execute menu = do
   putStr $ show menu
   prompt menu
 
-addItem :: Char -> Item a -> Menu a -> Menu a
+-- | Adds an 'Item' to a 'Menu', returning the resulting 'Menu'
+addItem
+  :: Char
+  -- ^ The character (must be upper case) of the 'Item' to be added to
+  -- the menu
+  -> Item a
+  -- ^ The 'Item' to be added
+  -> Menu a
+  -- ^ The 'Menu' being modified
+  -> Menu a
 addItem ch item menu =
   menu { items = Map.insert ch item $ items menu }
 
